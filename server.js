f3e2b82ccc76afb3c7be5d414c327fad2b9d4bc0
mpetty <michael.petty@gmail.com>
2019-04-11 17:16:01 -0700

    update api endpoint with shows endpoints

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // require express and other modules
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 // parse incoming urlencoded form data
 // and populate the req.body object
-const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // allow cross origin requests (optional)
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
@@ -43,18 +44,24 @@ app.get('/', function homepage(req, res) {
  */
 
 app.get('/api', (req, res) => {
-  // TODO: Document all your api endpoints below as a simple hardcoded JSON object.
-  // It would be seriously overkill to save any of this to your database.
-  // But you should change almost every line of this response.
   res.json({
-    woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
-    message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/example-username/express-personal-api/README.md", // CHANGE ME
-    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    title: "A view into MichaelP",
+    description: "a personal look into who is Michael Petty ",
+    contact: {
+      name: "API Support",
+      url: "http://www.test.com/support",
+      email: "no-reply@test.com"},
+    message: "Welcome to my personal api! Here's how you can use it!",
+    documentationUrl: "https://github.com/michaelpetty/express-personal-api/blob/master/README.md",
+    baseUrl: "https://peaceful-beach-38261.herokuapp.com/",
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "GET", path: "/api/profile", description: "Data about me"},
+      {method: "GET", path: "/api/shows", description: "Index of all theatre productions"},
+      {method: "GET", path: "/api/shows/:showId", description: "Show specifics of one theatre production"},
+      {method: "POST", path: "/api/shows", description: "Create a new theatre production"},
+      {method: "PUT", path: "/api/shows/:showId", description: "Update one specific theatre production"},
+      {method: "DELETE", path: "/api/shows/:showId", description: "Delete one theatre production"}
     ]
   })
 });
@@ -65,5 +72,5 @@ app.get('/api', (req, res) => {
 
 // listen on the port that Heroku prescribes (process.env.PORT) OR port 3000
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Express server is up and running on http://localhost:3000/');
+  console.log('Personal API server is up and running');
 });
